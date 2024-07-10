e51186c6f32697e573aded8b95c4e6a97fb3969b
Jason-c98 <cruz_Jason3@hotmail.com>
2024-07-10 10:56:49 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    assetPrefix: process.env.ASSET_PREIX,
+    basePath: process.env.BASE_PATH
+};
 
 export default nextConfig;
